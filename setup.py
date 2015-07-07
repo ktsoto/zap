41205be13383cd3ce94f952b242f08364f932651
Simon Conseil <simon.conseil@univ-lyon1.fr>
2015-07-07 20:30:23 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zap',
+    version='0.6-dev',
+    description=('ZAP (the Zurich Atmosphere Purge) is a high precision sky'
+                 ' subtraction tool'),
+    author='Kurt Soto',
+    author_email='sotok@phys.ethz.ch',
+    # url='',
+    # license='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['numpy', 'scipy', 'astropy'],
+    extras_require={'plot': ['matplotlib']},
+)
