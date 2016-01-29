65d41d9e281907712e188be4f0f613f11314d777
Simon Conseil <simon.conseil@univ-lyon1.fr>
2016-01-29 12:15:28 +0100

    Add more arguments to the script.

--- zap/__main__.py
+++ zap/__main__.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import sys
+import traceback
 
 from .version import __version__
 from .zap import process
@@ -14,29 +15,49 @@ def main():
         description='ZAP (the Zurich Atmosphere Purge) is a high precision sky'
                     ' subtraction tool.'
     )
-    parser.add_argument('incube', help='Input datacube path')
-    parser.add_argument('--version', '-V', action='version',
-                        version='%(prog)s ' + __version__)
-    parser.add_argument('--debug', '-d', action='store_true',
-                        help='Show debug info')
-    parser.add_argument('--no-clean', action='store_true',
-                        help='Disable NaN values interpolation')
-    parser.add_argument('--outcube', '-o', default='DATACUBE_FINAL_ZAP.fits',
-                        help='Output datacube path')
-    parser.add_argument('--skycube', help='Sky datacube path')
-    parser.add_argument('--cfwidth', type=int, default=100,
-                        help='Window size for the continuum filter')
+    addarg = parser.add_argument
+    addarg('incube', help='Input datacube path')
+    addarg('--version', '-V', action='version',
+           version='%(prog)s ' + __version__)
+    addarg('--debug', '-d', action='store_true',
+           help='show debug info')
+    addarg('--no-clean', action='store_true',
+           help='disable NaN values interpolation')
+    addarg('--outcube', '-o', default='DATACUBE_FINAL_ZAP.fits',
+           help='output datacube path')
+    addarg('--skycube',
+           help='Sky datacube path')
+    addarg('--svdoutput', default='ZAP_SVD.fits',
+           help='output SVD FITS file')
+    addarg('--extsvd',
+           help='Path of an input FITS file containing a SVD computed in a '
+           'previous step')
+    addarg('--cfwidthEV', type=int, default=100,
+           help='window size for the continuum filter for the SVD computation')
+    addarg('--cfwidthSP', type=int, default=50,
+           help='window size for the continuum filter')
+    addarg('--zlevel', default='median',
+           help='method for the zeroth order sky removal: none, sigclip or '
+           'median')
+    addarg('--cftype', default='weight',
+           help='method for the continuum filter: median or weight. For the '
+           'weight method, a zeroth order sky is required (see zlevel)')
     args = parser.parse_args()
 
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
 
     try:
-        process(args.incube, outcubefits=args.outcube, clean=not args.no_clean,
-                skycubefits=args.skycube, cfwidth=args.cfwidth)
+        process(
+            args.incube, outcubefits=args.outcube, clean=not args.no_clean,
+            skycubefits=args.skycube, svdoutputfits=args.svdoutput,
+            extSVD=args.extsvd, cfwidthEV=args.cfwidthEV,
+            cfwidthSP=args.cfwidthSP, zlevel=args.zlevel, cftype=args.cftype)
     except KeyboardInterrupt:
         sys.exit('Interrupted!')
     except Exception as e:
+        if args.debug:
+            traceback.print_exc()
         sys.exit('Failed to process file: %s' % e)
 
 
